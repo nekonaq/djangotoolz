ad02d39d8483becf7fc92890fa4487d1c4e31c18
Tatsuo Nakajyo <tnak@nekonaq.com>
2020-08-31 07:31:27 +0000

    shapshot 2008.31; レポート機能

---
+++ djangoexpack/utils/fromisocalendar.py
@@ -0,0 +1,59 @@
+import datetime
+from datetime import MINYEAR, MAXYEAR
+# from datetime import MINYEAR, MAXYEAR, _ymd2ord, _ord2ymd, _is_leap, _isoweek1monday
+
+
+def _isoweek1monday(year):
+    # Helper to calculate the day number of the Monday starting week 1
+    # XXX This could be done more efficiently
+    THURSDAY = 3
+    # firstday = _ymd2ord(year, 1, 1)
+    firstday = datetime.datetime(year, 1, 1).toordinal()
+    firstweekday = (firstday + 6) % 7  # See weekday() above
+    week1monday = firstday - firstweekday
+    if firstweekday > THURSDAY:
+        week1monday += 7
+    return week1monday
+
+
+def _is_leap(year):
+    "year -> 1 if leap year, else 0."
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+# backported from cpython for python3.6
+def fromisocalendar(year, week, day):
+    """Construct a date from the ISO year, week number and weekday.
+
+    This is the inverse of the date.isocalendar() function"""
+    # Year is bounded this way because 9999-12-31 is (9999, 52, 5)
+    if not MINYEAR <= year <= MAXYEAR:
+        raise ValueError(f"Year is out of range: {year}")
+
+    if not 0 < week < 53:
+        out_of_range = True
+
+        if week == 53:
+            # ISO years have 53 weeks in them on years starting with a
+            # Thursday and leap years starting on a Wednesday
+            # first_weekday = _ymd2ord(year, 1, 1) % 7
+            first_weekday = datetime.datetime(year, 1, 1).toordinal() % 7
+            if (first_weekday == 4 or (first_weekday == 3 and
+                                       _is_leap(year))):
+                out_of_range = False
+
+        if out_of_range:
+            raise ValueError(f"Invalid week: {week}")
+
+    if not 0 < day < 8:
+        raise ValueError(f"Invalid weekday: {day} (range is [1, 7])")
+
+    # Now compute the offset from (Y, 1, 1) in days:
+    day_offset = (week - 1) * 7 + (day - 1)
+
+    # Calculate the ordinal day for monday, week 1
+    day_1 = _isoweek1monday(year)
+    ord_day = day_1 + day_offset
+
+    # return datetime.datetime(*_ord2ymd(ord_day))
+    return datetime.datetime.fromordinal(ord_day)
--- djangoexpack/utils/timeutils.py
+++ djangoexpack/utils/timeutils.py
@@ -1,9 +1,52 @@
 import datetime
+import dateutil.parser
 import pytz
+import re
+from django.utils import timezone
 
-# local_tz = pytz.timezone('Asia/Tokyo')     # XXX 仮
+
+if hasattr(datetime.datetime, 'fromisocalendar'):
+    fromisocalendar = datetime.datetime.fromisocalendar
+else:
+    from .fromisocalendar import fromisocalendar  # noqa: F401
+
+
+def zstrftime(value):
+    """ datetime を elasticsearch のタイムスタンプ形式に変換する。
+
+    :param datetime.datetime value:	; utc であること
+    :rtype: str
+    """
+    return '{:%Y-%m-%dT%H:%M:%S}.{:03d}Z'.format(value, int(value.microsecond / 1000))
 
 
 def datetime_fromtimestamp(timestamp):
-    # return datetime.datetime.fromtimestamp(timestamp, tz=pytz.utc).astimezone(local_tz)
+    """ timestamp を UTC の tzaware な datetime に変換する。
+    """
     return datetime.datetime.fromtimestamp(timestamp, tz=pytz.utc)
+
+
+TZINFOS = {
+    'UTC': timezone.utc,
+}
+
+
+def parse_timestamp(timestamp):
+    """ elasticsearch の timestamp 文字列を aware datetime に変換する
+
+    :param str timestamp:                  # e.g. '2020-07-06T15:00:00.000Z'
+    """
+    return dateutil.parser.parse(timestamp, tzinfos=TZINFOS)
+
+month_re = re.compile(
+    r'(?P<year>\d{4})-(?P<month>\d{1,2})$'
+)
+
+def parse_month(value):
+    """ 文字列を解析して日付 (datetime.date) を返す。
+    value は "YYYY-MM" の形式
+    """
+    match = month_re.match(value)
+    if match:
+        kw = {key: int(val) for key, val in match.groupdict().items()}
+        return datetime.date(**kw, day=1)
